2d79fbf0d46688d4fb229a349911ca8c674178c9
Teneshwar Dwivedi <teneshwar9752@gmail.com>
2025-09-09 10:45:49 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -45,7 +45,6 @@ exports.login = async (req, res) => {
         if (!user) {
             return res.status(400).json({ msg: 'Invalid Credentials' });
         }
-
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ msg: 'Invalid Credentials' });
